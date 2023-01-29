406e577952c55260e5212b3ef3d34168c94ebd94
sinan akyol <110792519+sinanakyol@users.noreply.github.com>
2023-01-30 00:34:05 +0300

    menu - changes

--- js/index.js
+++ js/index.js
@@ -4,3 +4,10 @@
 // bar.addEventListener("click", () => {
 //   links.classList.toggle("show-bar");
 // });
+
+const menuToggle = document.querySelector("#menuToggle");
+const menu = document.querySelector("#menu");
+
+menuToggle.addEventListener("click", () => {
+  menu.classList.toggle("hide-bar");
+});
